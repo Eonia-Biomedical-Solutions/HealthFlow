2969653da49b7a9132a70f614d6c539b60101294
unlikeghost <unlikeghost@protonmail.com>
2024-02-19 19:56:44 -0600

    mongo db integration

--- app/routes/download.py
+++ app/routes/download.py
@@ -1,14 +1,27 @@
-import os
-from flask import (send_file, redirect, flash)
+import csv
+from io import StringIO
+from flask import (Response, redirect, flash)
+from app import db
 from . import (routes_bp)
 
 
-@routes_bp.route('/get/<project>', methods=['GET'])
-async def download(project: str):
-    dbfile: str = os.path.join('projects', project, 'db.sqlite')
-    # return dbfile
-    try:
-        return send_file(dbfile, as_attachment=True)
-    except FileNotFoundError:
-        flash('Archivo no encontrado, intente mas tarde', 'error')
-        return redirect('/')
+@routes_bp.route('/get/<project>/<version>', methods=['GET'])
+async def download(project: str, version: str):
+    collection_name: str = "{}-{}".format(project, version)
+    data = db.get(collection_name)
+    if len(data):
+        csv_output = StringIO()
+        headers = data[0].keys() if data else []
+        writer = csv.DictWriter(csv_output, fieldnames=headers)
+        writer.writeheader()
+        for doc in data:
+            writer.writerow(doc)
+        csv_output.seek(0)
+        return Response(
+                csv_output,
+                mimetype="text/csv",
+                headers={"Content-disposition":f"attachment; filename={project}-{version}.csv"}
+        )
+
+    flash('El proyecto aun no tiene datos! intente mas tarde', 'error')
+    return redirect('/')
